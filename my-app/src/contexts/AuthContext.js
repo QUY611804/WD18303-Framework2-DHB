4b8f3f900d4a2e98475247845760288d15c4fcbc
nguyentung04 <150757651+nguyentung04@users.noreply.github.com>
2024-08-07 13:11:18 +0700

    .

--- my-app/src/contexts/AuthContext.js
+++ my-app/src/contexts/AuthContext.js
@@ -1,84 +1,29 @@
-// src/contexts/AuthContext.js
-import React, { createContext, useContext, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { userAccounts } from "./userAccounts";
+// Đầu tiên, hãy tạo một Auth Context để quản lý trạng thái xác thực.
+import React, { createContext, useState, useContext } from 'react';
+//' createContext()' tạo một đối tượng ngữ cảnh mới có tên là 'AuthContext'. Đối tượng này sẽ giữ trạng thái xác thực và cung cấp các phương thức để thay đổi trạng thái đó.
+const AuthContext = createContext();
 
-const AuthContext = createContext(); //`AuthContext`:Bối cảnh này sẽ cung cấp dữ liệu và chức năng liên quan đến xác thực trong toàn bộ ứng dụng.
-
-// `useAuth`: Một hook tùy chỉnh giúp đơn giản hóa việc truy cập các giá trị ngữ cảnh xác thực.
-export const useAuth = () => {
-  return useContext(AuthContext);
-};
-
-/**
-   
-    * `AuthProvider`: Một thành phần React cung cấp các giá trị ngữ cảnh xác thực cho các thành phần con của nó.
-    * `user`: Biến trạng thái để lưu trữ người dùng đã xác thực hiện tại.
-    * `navigate`: Chức năng lập trình để điều hướng đến các tuyến đường khác nhau.
-    * `useEffect`: Khi gắn thành phần, nó sẽ kiểm tra xem có đối tượng người dùng nào được lưu trữ trong bộ nhớ cục bộ hay không và thiết lập trạng thái cho phù hợp.
- */
+// Thành phần này 'AuthProvider' là thành phần bao bọc sẽ cung cấp ngữ cảnh xác thực cho các thành phần con của nó.
+// `useState(false)` khởi tạo một biến trạng thái được đặt tên `isAuthenticated` với giá trị mặc định là `false`. Trạng thái này sẽ theo dõi xem người dùng đã được xác thực hay chưa.
+// `setIsAuthenticated` là một chức năng cho phép cập nhật `isAuthenticated` trạng thái.
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []);
-
-  /**
-   
-   * `login`: Một chức năng để xử lý việc đăng nhập của người dùng.
-   * `account`: Tìm tài khoản người dùng khớp với email và mật khẩu được cung cấp.
-   * Nếu tìm thấy tài khoản :
-        `setUser(account)`: Cập nhật `user` trạng thái.
-        `localStorage.setItem('user', JSON.stringify(account))`: Lưu dữ liệu người dùng vào bộ nhớ cục bộ.
-        `navigate('/admin')`: Chuyển hướng đến trang quản trị nếu người dùng là quản trị viên.
-        `navigate('/*')`: Chuyển hướng đến khu vực khách hàng để thực hiện các vai trò khác.
-   * Nếu không tìm thấy tài khoản : Hiển thị cảnh báo về thông tin đăng nhập không hợp lệ.
-
-   */
-  const login = (email, password) => {
-    const account = userAccounts.find(
-      (user) => user.email === email && user.password === password
-    );
-
-    if (account) {
-      setUser(account);
-      localStorage.setItem("user", JSON.stringify(account));
-      if (account.role === "admin") {
-        navigate("/admin");
-      } else {
-        navigate("/client");
-      }
-    } else {
-      alert("email và mật khẩu không chính xác");
-    }
-  };
+//   `login` là một hàm thiết lập ``isAuthenticated``trạng thái thành `true`, biểu thị người dùng đã được xác thực.
+// `logout` là một hàm thiết lập `isAuthenticated`trạng thái thành `false`, biểu thị người dùng chưa được xác thực.
+  const login = () => setIsAuthenticated(true);
+  const logout = () => setIsAuthenticated(false);
 
 
-  /**
-   * `logout`: Một chức năng để xử lý việc đăng xuất của người dùng.
-   * `setUser(null)`: Xóa `user` trạng thái.
-   * `localStorage.removeItem('user'):` Xóa dữ liệu người dùng khỏi bộ nhớ cục bộ.
-   * `navigate('/login')`: Chuyển hướng đến trang đăng nhập.
-   */
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem("user");
-    navigate("/login");
-  };
-
-
-  /**
-   * `AuthContext`.Provider: Cung cấp các giá trị user, login, và logoutcho các thành phần con của nó.
-   * {children}: Hiển thị các thành phần được bao bọc bởi `AuthProvider`.
-   *  */
+// Thành `AuthProvider` phần trả về thành `AuthContext.Provider`phần.
+// Thành `AuthContext.Provider`phần sử dụng valueprop để truyền `isAuthenticated`trạng thái và logincác `logout`hàm cho bất kỳ thành phần nào sử dụng ngữ cảnh này.
+// `{children}`đảm bảo rằng bất kỳ thành phần con nào của `AuthProvider`sẽ được hiển thị trong `AuthContext.Provider`.
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
+// `useAuth`là một hook tùy chỉnh bao bọc `useContext`hook, cho phép các thành phần truy cập vào ngữ cảnh xác thực dễ dàng hơn.
+//` useContext(AuthContext)`trả về giá trị ngữ cảnh hiện tại cho `AuthContext`, bao gồm `isAuthenticated`, `login`, và `logout`.
+export const useAuth = () => useContext(AuthContext);
